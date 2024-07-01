240a713aa78bfeddf08a5bcc4b05c9b15d873194
ShinMyeongseop28 <dkrntm2714@gmail.com>
2024-07-01 17:49:05 +0900

    사원정보 삽입,수정,삭제 신규사원 등록

--- 05.MVC/src/main/java/controller/FrontController.java
+++ 05.MVC/src/main/java/controller/FrontController.java
@@ -10,7 +10,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import common.Command;
+import hr.EmployeeDeleteCommand;
+import hr.EmployeeInfo;
+import hr.EmployeeInsert;
 import hr.EmployeeListCommand;
+import hr.EmployeeUpdate;
 import model.EmployeeDTO;
 import model.HrDAO;
 
@@ -36,29 +40,46 @@ public class FrontController extends HttpServlet {
 			//응답화면 연결
 			view = "hr/list.jsp";
 			
+		}else if (uri.equals("/register.do")) {
+			//신규사원입력화면 요청
+			
+			view = "hr/register.jsp";
 		}else if (uri.equals("/insert.do")) {
 			//비지니스 로직
+			Command command = new EmployeeInsert();
+			command.execute(request, response);
 			
 			//응답화면 연결
-			view = "";
+			view = "select.do";
 			redirect = true;
 			
 		}else if (uri.equals("/update.do")) {
 			//비지니스 로직
+			new EmployeeUpdate().execute(request, response);
 			
 			//응답화면 연결
-			view = "";
+			view = "info.do?id=" + request.getParameter("employee_id");
 			redirect = true;
 			
 		}else if (uri.equals("/delete.do")) {
 			//비지니스 로직
 			//DB에서 삭제처리
 			System.out.println("삭제처리");
+			Command command = new EmployeeDeleteCommand();
+			command.execute(request, response);
 			
 			//응답화면 연결
-			view = "hr/list.jsp";
 			redirect = true;
+			view = "select.do";
+		}else if(uri.equals("/info.do")) {
+			new EmployeeInfo().execute(request, response);
 			
+			//응답화면
+			view = "hr/info.jsp";
+		}else if (uri.equals("/modify.do")) {
+			new EmployeeInfo().execute(request, response);
+			
+			view = "hr/modify.jsp";
 		}
 		if( redirect ) {
 			response.sendRedirect(view);
--- 05.MVC/src/main/java/model/HrDAO.java
+++ 05.MVC/src/main/java/model/HrDAO.java
@@ -3,16 +3,108 @@ package model;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.sql.DataSource;
 
+import org.apache.jasper.tagplugins.jstl.core.Catch;
+
 public class HrDAO {
 	private Connection conn;
 	private PreparedStatement ps;
 	private ResultSet rs;
+	
+	//신규사원정보 저장
+	public void insertEmployee(EmployeeDTO dto) {
+		connect();
+		
+		String sql = "insert into employees"
+				+ "(employee_id, last_name, first_name, email, job_id, hire_date) "
+				+ "values ( employees_seq.nextval, ?, ?, ?, ?, ? )";
+		try {
+			ps = conn.prepareStatement(sql);
+			ps.setString(1, dto.getLast_name());
+			ps.setString(2, dto.getFirst_name());
+			ps.setString(3, dto.getEmail());
+			ps.setString(4, dto.getJob_id());
+			ps.setString(5, dto.getHire_date());
+			ps.executeUpdate();
+			
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		} finally {
+			disconnect();
+		}
+		
+	}
+	
+	//사원정보 변경저장
+	public void updateEmployee(EmployeeDTO dto) {
+		connect();
+		
+		String sql = "update employees set last_name = ?,"
+				+ " first_name = ? where employee_id = ?";
+		try {
+			ps = conn.prepareStatement(sql);
+			ps.setString(1, dto.getLast_name());
+			ps.setString(2, dto.getFirst_name());
+			ps.setInt(3, dto.getEmployee_id());
+			ps.executeUpdate();
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		} finally {
+			disconnect();
+		}
+		
+	}
+	
+	//사원정보조회
+	public EmployeeDTO getOneEmployee(int employee_id) {
+		connect();
+		
+		EmployeeDTO dto = null;
+		String sql = "select * from employees where employee_id = ?";
+		try {
+			ps = conn.prepareStatement(sql);
+			ps.setInt(1, employee_id);
+			rs = ps.executeQuery();
+			if(rs.next()) {
+				dto = new EmployeeDTO();
+				dto.setEmployee_id(rs.getInt("employee_id"));
+				dto.setFirst_name(rs.getString("first_name"));
+				dto.setLast_name(rs.getString("last_name"));
+			}
+			
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		} finally {
+			disconnect();
+		}
+		return dto;
+		
+	} 
+	
+	//사원정보삭제
+	public int deleteEmployee(int employee_id) {
+		connect();
+		
+		int dml = 0;
+		String sql = "delete from employees where employee_id = ?";
+		try {
+			ps = conn.prepareStatement(sql);
+			ps.setInt(1, employee_id);
+			//ps.executeQuery();	//select 쿼리실행
+			dml = ps.executeUpdate();	//insert, update, delete 쿼리실행
+			
+		} catch (SQLException e) {
+		} finally {
+			disconnect();
+		}
+		return dml;
+	}
 	
 	//사원목록조회
 	public ArrayList<EmployeeDTO> listOfEmployee() {
