75f7d83763ff9dcb42767dde08696279d9d6ada8
ShinMyeongseop28 <dkrntm2714@gmail.com>
2024-07-11 17:49:37 +0900

    mybatis 적용한 고객관리-목록,정보,수정

---
+++ 09.Mybatis/src/main/java/controller/CustomerController.java
@@ -0,0 +1,74 @@
+package controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import customer.ListCommand;
+import customer.UpdateCommand;
+import customer.infoCommand;
+
+/**
+ * Servlet implementation class CustomerController
+ */
+@WebServlet("*.cu")
+public class CustomerController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String uri = request.getServletPath();
+		String view = null;
+		boolean redirect = false;
+		
+		//고객 목록화면 요청
+		if(uri.equals("/list.cu")) {
+			//비지니스 로직
+			new ListCommand().execute(request, response);
+			
+			//응답화면
+			view = "customer/list.jsp";
+		}
+		
+		//고객 정보화면 요청
+		else if(uri.equals("/info.cu")) {
+			//비지니스 로직
+			new infoCommand().execute(request, response);
+			
+			//응답화면
+			view = "customer/info.jsp";
+		}
+		
+		//고객정보 수정화면 요청
+		else if(uri.equals("/modify.cu")) {
+			//비지니스 로직
+			new infoCommand().execute(request, response);
+			
+			//응답화면
+			view = "customer/modify.jsp";
+		}
+		
+		//고객정보 수정저장 처리 요청
+		else if(uri.equals("/update.cu") ) {
+			//비지니스 로직
+			new UpdateCommand().execute(request, response);
+			
+			//응답화면
+			view = "info.cu?id=" + request.getParameter("id");
+			redirect = true;
+		}
+		
+		if (redirect)
+			response.sendRedirect(view);
+		else
+			request.getRequestDispatcher(view).forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
---
+++ 09.Mybatis/src/main/java/customer/infoCommand.java
@@ -0,0 +1,20 @@
+package customer;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import Common.Command;
+import customer.model.CustomerDAO;
+import customer.model.CustomerDTO;
+
+public class infoCommand implements Command{
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) {
+		//비지니스 로직
+		//선택한 고객정보를 DB에서 조회해오기 -> 고객정보조회 메소드 호출
+		int id = Integer.parseInt(request.getParameter("id"));
+		CustomerDTO dto = new CustomerDAO().getOneCustomer(id);
+		//화면에 출력할 수 있게 request에 저장하기
+		request.setAttribute("dto", dto);
+	}
+}
